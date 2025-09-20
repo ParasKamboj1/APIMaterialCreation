20c34047cd1c6a4d492d76e711a6d993ef99e03c
Paras Kamboj <paras1234567899@gmail.com>
2025-09-20 16:59:35 +0530

    done

---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI,HTTPException,Body
+from pymongo import MongoClient
+import pydanticModels as models
+
+app = FastAPI()
+
+client = MongoClient("mongodb://localhost:27017/")
+
+database = client["MaterialsBandM"]
+
+MODEL_COLLECTION_MAP = {
+    "page_one" : (models.PageOneModel,database["page_one"]),
+    "page_two" : (models.PageTwoModel,database["page_two"]),
+    "page_three" : (models.PageThreeModel,database["page_three"]),
+    "page_four" : (models.PageFourModel,database["page_four"]),
+    "page_five" : (models.PageFiveModel,database["page_five"]),
+    "page_six" : (models.PageSixModel,database["page_six"]),
+    "page_seven" : (models.PageSevenModel,database["page_seven"]),
+    "page_eight" : (models.PageEightModel,database["page_eight"]),
+    "page_nine" : (models.PageNineModel,database["page_nine"])
+}
+
+
+
+@app.post("/create/{page_name}")
+def create_page_data(page_name : str , data: dict = Body(...)):
+    if page_name not in MODEL_COLLECTION_MAP:
+        raise HTTPException(status_code=400,detail="Invalid Page Name")
+    model,collection = MODEL_COLLECTION_MAP[page_name]
+    validated_data = model(**data).model_dump()
+    result = collection.insert_one(validated_data)
+    return {"inserted_data" : str(result)}
+
+@app.get("/get/completedata/{page_number}")
+def get_documents(page_number : str):
+    if page_number not in MODEL_COLLECTION_MAP:
+        raise HTTPException(status_code=400,detail="Invalid Page Number")
+    _,collection = MODEL_COLLECTION_MAP[page_number]
+    docs = []
+    for d in collection.find():
+        d["_id"] = str(d["_id"])
+        docs.append(d)
+    return docs 
+
+@app.put("/update/pagenumber/{page_name2}")
+def update_document_partial(page_name2 : str , data : dict = Body(...)):
+   if page_name2 not in MODEL_COLLECTION_MAP:
+        raise HTTPException(status_code=400,detail="Invalid Page Number")
+   model,collection = MODEL_COLLECTION_MAP[page_name2]
+   if "id" not in data or "material" not in data:
+        raise HTTPException(status_code=400, detail="'id' and 'material' are required to update")
+   filter_query = {"id" : data["id"] , "material" : data["material"]}
+   validated_data = model(**data).model_dump(exclude_unset = True)
+   result = collection.update_one(filter_query,{"$set":validated_data})
+
+   if result.matched_count == 0:
+        raise HTTPException(status_code=404, detail="Document not found")
+
+   return {"message": "Document updated successfully"}
